6a4e884fd289de991119aee1ae70b7b26a654a62
sofia ongele <sofiaongele@gmail.com>
2018-07-11 18:25:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+driver = {name: 'Sam', address: '11 Broadway'}
+
+// when using Object.assign() the {} creates a new object, the next thing is the reference and the final thing is what you append
+
+function updateDriverWithKeyAndValue (driver, key, value) {
+  return Object.assign({}, driver, { [key]: value }); 
+}
+
+function destructivelyUpdateDriverWithKeyAndValue (driver, key, value) {
+  driver[key] = value;
+
+  return driver;
+}
+
+function deleteFromDriverByKey (driver, key) {
+  const newObj = Object.assign({}, driver);
+
+  delete newObj[key];
+
+  return newObj;
+}
+
+function destructivelyDeleteFromDriverByKey (driver, key) {
+  delete driver[key];
+
+  return driver;
+}
